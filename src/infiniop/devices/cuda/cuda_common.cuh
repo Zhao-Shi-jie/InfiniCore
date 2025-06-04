489b15dfa8a472f2544b83786bb363d79038f869
zhaoshijie <zhao_shjie@163.com>
2025-06-04 22:49:01 +0800

    add CHECK_CUSPARSE

--- src/infiniop/devices/cuda/cuda_common.cuh
+++ src/infiniop/devices/cuda/cuda_common.cuh
@@ -3,11 +3,23 @@
 
 #include "cuda_handle.cuh"
 #include "infinicore.h"
+#include <cuda_runtime.h>
+#include <cusparse.h>
 
 namespace device::cuda {
 
 cudnnDataType_t getCudnnDtype(infiniDtype_t dt);
 
 } // namespace device::cuda
+
+
+// cuSPARSE错误检查宏
+#define CHECK_CUSPARSE(call)                                            \
+    do {                                                                \
+        cusparseStatus_t err = call;                                    \
+        if (err != CUSPARSE_STATUS_SUCCESS) {                          \
+            return INFINI_STATUS_INTERNAL_ERROR;                       \
+        }                                                               \
+    } while (0)
 
 #endif // __INFINIOP_CUDA_COMMON_CUH__
